e7b399e9f9abb2d7790082641ccdf6d600f2f5e6
Eya <aya.selmi1@st.com>
2021-01-22 15:24:11 +0100

    Astyle update

--- astyle-check/scripts/astyle.py
+++ astyle-check/scripts/astyle.py
@@ -73,8 +73,7 @@ def checkAstyle():
 # Find all files in source root path
 def find_files():
     try:
-    output = subprocess.check_output(
-        ['git', 'diff', '--name-only', 'HEAD^'], stderr=subprocess.STDOUT)
+    output = subprocess.check_output(['git', 'diff', '--name-only', 'HEAD^'], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
        print("Exception on process, rc=", e.returncode, "output=", e.output)
 
