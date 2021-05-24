7c66f2c0335c3208bec8b525cff4af39865df217
Eya <aya.selmi1@st.com>
2021-05-24 13:09:32 +0100

    update script

--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -53,7 +53,7 @@ do
 
 
     if [ $a -eq 14 ]; then 
-      echo "*****Compilation on device "${PART_NUMBER}"xx*****" ;
+      echo " \n *****Compilation on device "${PART_NUMBER}"xx***** \n " ;
       for source in "${HAL_DIR}/Src"/*.c
       do
           # Log message to the user.
